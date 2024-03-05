cb2ec4b7f02489085926ddcc5db9a143ee7bbd30
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-03-05 16:10:30 -0300

    Update utils.mjs

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,8 +10,23 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
+
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
+}
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
