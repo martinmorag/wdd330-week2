0e42062ceba9f889d875c71df0f8b8057efa72fc
martinmorag <113954312+martinmorag@users.noreply.github.com>
2024-03-05 23:01:09 -0300

    Update utils.mjs

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -10,9 +10,14 @@ export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
-
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
+}
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
 }
 export function renderListWithTemplate(
   templateFn,
